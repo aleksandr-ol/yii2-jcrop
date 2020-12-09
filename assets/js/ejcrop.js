b89a0e3d5365fb5730d79beccdceb710c4ee0789
aleksandr-ol <oleksandr.oleniuk@gmail.com>
2020-12-09 10:06:02 +0200

    add ejcrop_changeOptions

--- assets/js/ejcrop.js
+++ assets/js/ejcrop.js
@@ -145,10 +145,5 @@ function ejcrop_changeOptions(id, options) {
         $('#start_' + id).show();
         $('#crop_' + id + ', #cancel_' + id).hide();
         ajaxRequest(id);
-        if (!jcrop.id) {
-            jcrop.id = $.Jcrop('#' + id, options);
-        }
-        jcrop.id.release();
-        jcrop.id.disable();
     });
 }
